3a28fde38d9a0c9d0f98e06b3a641f0d9e326c65
Finn Vos <f.vos@dr-flex.de>
2022-03-21 10:18:04 +0100

    Add L

--- best_code_ever.rs
+++ best_code_ever.rs
@@ -9,7 +9,7 @@ const SPACE: &str = " ";
 
 fn main() {
     println!(
-        "{}{}{}{}{}{}{}{}{}",
-        H, E, L, L, O, SPACE, W, O, R
+        "{}{}{}{}{}{}{}{}{}{}",
+        H, E, L, L, O, SPACE, W, O, R, L
     );
 }
