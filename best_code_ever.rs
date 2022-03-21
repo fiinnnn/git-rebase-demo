cfb8bf4155fc249ecd6e1e53db6f253c814ddc35
Finn Vos <f.vos@dr-flex.de>
2022-03-21 10:10:50 +0100

    Add code

---
+++ best_code_ever.rs
@@ -0,0 +1,15 @@
+const H: &str = "H";
+const E: &str = "e";
+const L: &str = "l";
+const O: &str = "o";
+const W: &str = "W";
+const R: &str = "r";
+const D: &str = "d";
+const SPACE: &str = " ";
+
+fn main() {
+    println!(
+        "{}{}{}{}{}{}{}{}{}{}{}",
+        H, E, L, L, O, SPACE, W, O, R, L, D
+    );
+}
